fa56c7ec3c460cf3bc57f6e4f54b63cb712f8e00
Esteban Ochoa <esteban8adela@gmail.com>
2025-09-05 21:14:21 -0600

    Agregue mensaje de error si el numero de npc > a las combinaciones posibles

--- Assets/Scripts/NPCSpawner.cs
+++ Assets/Scripts/NPCSpawner.cs
@@ -50,107 +50,115 @@ public class NPCSpawner : MonoBehaviour
         Renderer planeRenderer = spawnPlane.GetComponent<Renderer>();
         Bounds planeBounds = planeRenderer.bounds;
 
-        //Loop para crear NPCs
-        for (int i = 0; i < numberOfNPC; i++)
+        if (numberOfNPC < (hatConfigs.Length * accessoryConfigs.Length * materials.Length))
         {
-            //Posicion random
-            float randomX = UnityEngine.Random.Range(planeBounds.min.x, planeBounds.max.x);
-            float randomZ = UnityEngine.Random.Range(planeBounds.min.z, planeBounds.max.z);
-
-            Vector3 randomSpawnPosition = new Vector3(randomX, planeBounds.max.y, randomZ);
-
-            //Crear NPCs
-            GameObject newNPC = Instantiate(npcPrefab, randomSpawnPosition, npcPrefab.transform.rotation);
-
-            // Asegurar etiqueta para detección emulada
-            try { newNPC.tag = "Person"; } catch { /* si la tag no existe o es inválida */ }
-
-            // Asegurar descriptor de persona para características aleatorias
-            var descriptor = newNPC.GetComponent<PersonDescriptor>();
-            if (descriptor == null) descriptor = newNPC.AddComponent<PersonDescriptor>();
-            descriptor.randomizeOnStart = true;
-
-            //Personalizar NPC
-            int materialIndex = UnityEngine.Random.Range(0, materials.Length);
-            Renderer npcRenderer = newNPC.GetComponentInChildren<Renderer>();
-            if (npcRenderer != null)
+            //Loop para crear NPCs
+            for (int i = 0; i < numberOfNPC; i++)
             {
-                npcRenderer.material = materials[materialIndex];
-            }
+                //Posicion random
+                float randomX = UnityEngine.Random.Range(planeBounds.min.x, planeBounds.max.x);
+                float randomZ = UnityEngine.Random.Range(planeBounds.min.z, planeBounds.max.z);
 
-            //Ponerles sombrerito
-            if (hatConfigs.Length > 0)
-            {
-                int hatIndex = UnityEngine.Random.Range(0, hatConfigs.Length);
-                HatConfig selectedHat = hatConfigs[hatIndex];
-                
-                GameObject hat = Instantiate(selectedHat.prefab);
+                Vector3 randomSpawnPosition = new Vector3(randomX, planeBounds.max.y, randomZ);
 
-                //Buscar el anclaje del sombrero
-                Transform hatAttachPoint = newNPC.transform.Find("HatAttachmentPoint");
+                //Crear NPCs
+                GameObject newNPC = Instantiate(npcPrefab, randomSpawnPosition, npcPrefab.transform.rotation);
 
-                if (hatAttachPoint != null)
+                // Asegurar etiqueta para detección emulada
+                try { newNPC.tag = "Person"; } catch { /* si la tag no existe o es inválida */ }
+
+                // Asegurar descriptor de persona para características aleatorias
+                var descriptor = newNPC.GetComponent<PersonDescriptor>();
+                if (descriptor == null) descriptor = newNPC.AddComponent<PersonDescriptor>();
+                descriptor.randomizeOnStart = true;
+
+                //Personalizar NPC
+                int materialIndex = UnityEngine.Random.Range(0, materials.Length);
+                Renderer npcRenderer = newNPC.GetComponentInChildren<Renderer>();
+                if (npcRenderer != null)
                 {
-                    hat.transform.SetParent(hatAttachPoint);
-                    
-                    if (useFBXCompatibilityMode)
+                    npcRenderer.material = materials[materialIndex];
+                }
+
+                //Ponerles sombrerito
+                if (hatConfigs.Length > 0)
+                {
+                    int hatIndex = UnityEngine.Random.Range(0, hatConfigs.Length);
+                    HatConfig selectedHat = hatConfigs[hatIndex];
+
+                    GameObject hat = Instantiate(selectedHat.prefab);
+
+                    //Buscar el anclaje del sombrero
+                    Transform hatAttachPoint = newNPC.transform.Find("HatAttachmentPoint");
+
+                    if (hatAttachPoint != null)
                     {
-                        // Modo compatibilidad: usar configuraciones manuales
-                        hat.transform.localPosition = selectedHat.localPosition;
-                        hat.transform.localRotation = Quaternion.Euler(selectedHat.localRotation);
-                        hat.transform.localScale = selectedHat.localScale;
+                        hat.transform.SetParent(hatAttachPoint);
+
+                        if (useFBXCompatibilityMode)
+                        {
+                            // Modo compatibilidad: usar configuraciones manuales
+                            hat.transform.localPosition = selectedHat.localPosition;
+                            hat.transform.localRotation = Quaternion.Euler(selectedHat.localRotation);
+                            hat.transform.localScale = selectedHat.localScale;
+                        }
+                        else
+                        {
+                            // Modo estándar: resetear transformaciones
+                            hat.transform.localPosition = Vector3.zero;
+                            hat.transform.localRotation = Quaternion.identity;
+                            hat.transform.localScale = Vector3.one;
+                        }
                     }
                     else
                     {
-                        // Modo estándar: resetear transformaciones
-                        hat.transform.localPosition = Vector3.zero;
-                        hat.transform.localRotation = Quaternion.identity;
-                        hat.transform.localScale = Vector3.one;
+                        Debug.LogError("No se encontró 'HatAttachmentPoint' en el NPC: " + newNPC.name);
+                        Destroy(hat); // Limpiar el objeto huérfano
                     }
                 }
-                else
+
+                //Ponerle accesorios uwu
+                if (accessoryConfigs.Length > 0)
                 {
-                    Debug.LogError("No se encontró 'HatAttachmentPoint' en el NPC: " + newNPC.name);
-                    Destroy(hat); // Limpiar el objeto huérfano
-                }
-            }
+                    int accessoryIndex = UnityEngine.Random.Range(0, accessoryConfigs.Length);
+                    AccessoryConfig selectedAccessory = accessoryConfigs[accessoryIndex];
 
-            //Ponerle accesorios uwu
-            if (accessoryConfigs.Length > 0)
-            {
-                int accessoryIndex = UnityEngine.Random.Range(0, accessoryConfigs.Length);
-                AccessoryConfig selectedAccessory = accessoryConfigs[accessoryIndex];
-                
-                GameObject accessory = Instantiate(selectedAccessory.prefab);
+                    GameObject accessory = Instantiate(selectedAccessory.prefab);
 
-                //Buscar anclaje de accesorio
-                Transform accessoryAttachPoint = newNPC.transform.Find("AccessoryAttachmentPoint");
+                    //Buscar anclaje de accesorio
+                    Transform accessoryAttachPoint = newNPC.transform.Find("AccessoryAttachmentPoint");
 
-                if (accessoryAttachPoint != null)
-                {
-                    accessory.transform.SetParent(accessoryAttachPoint);
-                    
-                    if (useFBXCompatibilityMode)
+                    if (accessoryAttachPoint != null)
                     {
-                        // Modo compatibilidad: usar configuraciones manuales
-                        accessory.transform.localPosition = selectedAccessory.localPosition;
-                        accessory.transform.localRotation = Quaternion.Euler(selectedAccessory.localRotation);
-                        accessory.transform.localScale = selectedAccessory.localScale;
+                        accessory.transform.SetParent(accessoryAttachPoint);
+
+                        if (useFBXCompatibilityMode)
+                        {
+                            // Modo compatibilidad: usar configuraciones manuales
+                            accessory.transform.localPosition = selectedAccessory.localPosition;
+                            accessory.transform.localRotation = Quaternion.Euler(selectedAccessory.localRotation);
+                            accessory.transform.localScale = selectedAccessory.localScale;
+                        }
+                        else
+                        {
+                            // Modo estándar: resetear transformaciones
+                            accessory.transform.localPosition = Vector3.zero;
+                            accessory.transform.localRotation = Quaternion.identity;
+                            accessory.transform.localScale = Vector3.one;
+                        }
                     }
                     else
                     {
-                        // Modo estándar: resetear transformaciones
-                        accessory.transform.localPosition = Vector3.zero;
-                        accessory.transform.localRotation = Quaternion.identity;
-                        accessory.transform.localScale = Vector3.one;
+                        Debug.LogError("No se encontró 'AccessoryAttachmentPoint' en el NPC: " + newNPC.name);
+                        Destroy(accessory); // Limpiar el objeto huérfano
                     }
                 }
-                else
-                {
-                    Debug.LogError("No se encontró 'AccessoryAttachmentPoint' en el NPC: " + newNPC.name);
-                    Destroy(accessory); // Limpiar el objeto huérfano
-                }
             }
+        }
+        else
+        {
+            Debug.LogError("Numero de NPC Mayor al numero de configuraciones posibles, favor de ingresar un numero menor a " + (hatConfigs.Length * accessoryConfigs.Length * materials.Length));
+            return;
         }
     }
 }
